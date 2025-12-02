924279abf9f4a58ae8286d31f0b506a14e7dc17f
Rela45 <alex.imbriani00@gmail.com>
2025-12-02 18:14:02 +0100

    first commit

---
+++ ChoiceMenu/Esercizi/Biblioteca/Documents.cs
@@ -0,0 +1,12 @@
+namespace Biblioteca
+{
+    public abstract class Documents
+    {
+        private string? _id;
+        private string? _title;
+        private string? _year;
+        private string? _sector;
+        private bool _disponibile;
+        
+    }
+}
---
+++ ChoiceMenu/Esercizi/Biblioteca/User.cs
@@ -0,0 +1,10 @@
+namespace Biblioteca
+{
+    public class User
+{
+    private string? _name;
+    private string? _surname;
+    private string? _email;
+    private string? _password;
+}
+}
